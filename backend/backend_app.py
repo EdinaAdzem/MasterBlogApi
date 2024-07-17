7b2837628d3938f2cda9c5b897df3e64c12a6133
EdinaAdzem <71989436+EdinaAdzem@users.noreply.github.com>
2024-07-17 11:38:16 +0200

    adding the udpate_post endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
@@ -26,7 +25,7 @@ def add_post():
             missing_fields.append('title')
         if 'content' not in data:
             missing_fields.append('content')
-        return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400 #handle errors
+        return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400  # handle errors
 
     # add a new post
     new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
@@ -38,29 +37,38 @@ def add_post():
     POSTS.append(new_post)
     return jsonify(new_post), 201
 
-#delete endpoint
+
+# delete endpoint
 @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
     """delete post endpoint"""
     global POSTS
-    #first attempt to find the post
+    # first attempt to find the post
     for post in POSTS:
         if post['id'] == post_id:
             POSTS.remove(post)
             return jsonify({"message": f"Post  id {post_id} has been deleted successfully."}), 200
-        #handle user entering the wrong id
+        # handle user entering the wrong id
     return jsonify({"error": f"Post id {post_id} does not exist!!!."}), 404
 
-@app.route('/api/posts/<int:post_id>', methods=['UPDATE'])
+
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
 def update_post(post_id):
     data = request.get_json()
-    update_post =None
+    update_post = None
 
-    #locate the posts
-    #perform the update on the title
-    #perform the update on the content
-    #handle the errors
+    # locate the posts
+    for post in POSTS:
+        post['title'] = data.get('title', post['title'])  # update title
+        post['content'] = data.get('content', post['content'])  # update content
+        updated_post = post
+        break
 
+    # handle the errors
+    if updated_post:
+        return jsonify(updated_post), 200
+    else:
+        return jsonify({"Some Error": f"post id {post_id}does not exist"}), 404
 
 
 if __name__ == '__main__':
